8bb52b1ab7adc6b77dc26b6ebdf04110d822aea7
Richie Osagie <richie0914@gmail.com>
2022-08-10 21:04:07 +0100

    Initial commit

---
+++ src/Perimeter.java
@@ -0,0 +1,9 @@
+public class Perimeter {
+    public static void main (String[] args) {
+        double width = 12.5;
+        double length = 15.3;
+
+        // Calculating the perimeter of shape
+        System.out.println(12.5*2+15.3*2);
+    }
+}
